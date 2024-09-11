7c4f22dbff00bbfd5d338bd403363fca4e32f7e0
llaurabatt <laura.battaglia@keble.ox.ac.uk>
2024-09-11 19:59:25 +0000

    5 items write configs for hpo

---
+++ configs/5_items_hpo_flow_nsf_vmp_flow_etaVMP.py
@@ -0,0 +1,192 @@
+"""Hyperparameter configuration."""
+
+import ml_collections
+
+
+def get_config():
+  """Get the hyperparameter configuration."""
+  config = ml_collections.ConfigDict()
+
+  config.method = 'hp_opt_vmp_flow_allhp'#'vmp_flow_allhp' # vmp_flow
+
+  # Dataset to use
+  config.dataset_id = 'coarsen_8_items'
+
+  # Defined in `flows.py`.
+  config.flow_name = 'meta_nsf'
+  config.cond_hparams_names = ['eta']
+  config.optim_prior_hparams_dir_fixed_eta = ''
+  config.eta_fixed = 1.0 # should still optimise eta
+  config.tune_vmp_hparams_fix_eta = False
+  config.tune_vmp_hparams = False
+
+  # kwargs to be passed to the flow
+  config.flow_kwargs = ml_collections.ConfigDict()
+  config.flow_kwargs.is_additive_flow = False
+  config.flow_kwargs.num_basis_gps = 10
+  config.flow_kwargs.inducing_grid_shape = (11, 11)
+  # Number of layers to use in the flow.
+  config.flow_kwargs.num_layers = 8
+  # Hidden sizes of the MLP conditioner.
+  config.flow_kwargs.hidden_sizes_conditioner = [30] * 5
+  # Hidden sizes of the MLP conditioner.
+  config.flow_kwargs.hidden_sizes_conditioner_eta = [30] * 5
+  # Number of bins to use in the rational-quadratic spline.
+  config.flow_kwargs.num_bins = 10
+  # the bounds of the quadratic spline transformer
+  config.flow_kwargs.spline_range = (-10., 10.)
+  # Ranges of posterior locations
+  # (NOTE: these will be modified in the training script)
+  config.flow_kwargs.loc_x_range = (0., 1.)
+  config.flow_kwargs.loc_y_range = (0., 0.8939394)
+
+  # Define priors
+  
+
+  config.prior_hparams = ml_collections.ConfigDict()
+  config.prior_hparams.mu_prior_concentration = 1.
+  config.prior_hparams.mu_prior_rate = 0.5
+  config.prior_hparams.zeta_prior_a = 1.
+  config.prior_hparams.zeta_prior_b = 1.
+  config.prior_hparams.w_prior_scale = 5.
+  config.prior_hparams.a_prior_scale = 10.
+  config.kernel_name = 'ExponentiatedQuadratic'
+  config.kernel_kwargs = ml_collections.ConfigDict()
+  config.kernel_kwargs.amplitude = 0.2
+  config.kernel_kwargs.length_scale = 0.3
+  config.gp_jitter = 1e-3
+
+  config.prior_hparams_hparams = ml_collections.ConfigDict()
+  config.prior_hparams_hparams.w_sampling_scale_alpha = 5.
+  config.prior_hparams_hparams.w_sampling_scale_beta = 1.
+  config.prior_hparams_hparams.a_sampling_scale_alpha = 10.
+  config.prior_hparams_hparams.a_sampling_scale_beta = 1.
+  config.prior_hparams_hparams.kernel_sampling_amplitude_alpha = 0.1
+  config.prior_hparams_hparams.kernel_sampling_amplitude_beta = 0.4
+  config.prior_hparams_hparams.kernel_sampling_lengthscale_alpha = 0.2
+  config.prior_hparams_hparams.kernel_sampling_lengthscale_beta = 0.5
+
+  # Number of training steps to run.
+  config.training_steps = 200_000 #300_000
+
+  # Optimizer.
+  config.optim_kwargs = ml_collections.ConfigDict()
+  config.optim_kwargs.grad_clip_value = 1.0
+  config.optim_kwargs.lr_schedule_name = 'warmup_exponential_decay_schedule'
+  config.optim_kwargs.lr_schedule_kwargs = ml_collections.ConfigDict()
+  config.optim_kwargs.lr_schedule_kwargs.init_value = 0.
+#   config.optim_kwargs.lr_schedule_kwargs.peak_value = 3e-4
+  config.optim_kwargs.lr_schedule_kwargs.warmup_steps = 3_000
+  config.optim_kwargs.lr_schedule_kwargs.transition_steps = 10_000
+#   config.optim_kwargs.lr_schedule_kwargs.decay_rate = 0.5
+  config.optim_kwargs.lr_schedule_kwargs.transition_begin = 0
+  config.optim_kwargs.lr_schedule_kwargs.staircase = False
+  config.optim_kwargs.lr_schedule_kwargs.end_value = None
+
+#   config.optim_kwargs.lr_schedule_kwargs = {
+#       'init_value': 0.,
+#       'peak_value': 3e-4,
+#       'warmup_steps': 3_000,
+#       'transition_steps': 10_000,
+#       'decay_rate': 0.5,
+#       'transition_begin': 0,
+#       'staircase': False,
+#       'end_value': None,
+#   }
+  
+
+  config.num_lp_anchor_train = 120
+  config.num_lp_floating_train = 10
+  config.num_items_keep = 5 #8
+  config.num_lp_anchor_val = 0
+  config.num_lp_anchor_test = 0
+  config.remove_empty_forms = True
+  config.ad_hoc_val_profiles = False
+
+  config.lp_floating_train = (136, 234, 1002, 501, 236, 237, 319, 515, 699, 755)
+
+  # Number of posteriors samples to approximate the variational loss (ELBO).
+  config.num_samples_elbo = 5
+  config.num_samples_gamma_profiles = 5
+
+  # How often to evaluate the model.
+  config.eval_steps = config.training_steps // 10 #5
+  config.num_samples_eval = 100
+  config.eval_last = False
+
+  config.max_steps_nan = 1_000
+
+  # How often to log images to monitor convergence.
+  config.log_img_steps = 0#config.training_steps // 5
+  config.log_img_at_end = False
+  config.save_samples = False
+
+  # Number of samples used in the plots.
+  config.num_samples_plot = 1_000
+  config.num_samples_save = 1_000
+  config.eta_save = [1.0]
+  config.num_samples_chunk_plot = 500
+
+  # Floating profiles to plot in grid
+  config.floating_anchor_copies = False # CHECK ALWAYS!!!
+  config.lp_floating_grid10 = [
+      136, 234, 1002, 501, 236, 237, 319, 515, 699, 755
+  ]
+  # config.lp_floating_grid10 = None
+  config.lp_random_anchor_10 = None
+
+  # eta shown in figures
+  config.eta_plot = [0.001, 0.250, 0.500, 0.750, 1.000]
+
+  # How often to save model checkpoints.
+  config.checkpoint_steps = 0 #config.training_steps // 5
+  config.save_last_checkpoint = True
+  # How many checkpoints to keep.
+  config.checkpoints_keep = 1
+
+  # Number of samples of eta for Meta-Posterior training
+  config.eta_sampling_a = 0.5
+  config.eta_sampling_b = 0.5
+
+  # Use random location for anchor profiles for evaluation
+  config.include_random_anchor = False
+  # Metric for Hyperparameter Optimization
+  config.synetune_metric = "mean_dist_anchor_val_min"
+  config.path_mcmc_img = ''
+  config.path_MCMC_samples = ''
+  config.max_wass_samples = 1_000
+  # Random seed
+  config.seed = 1
+
+  config.use_wandb = True
+  config.fixed_configs_wandb = {
+            # "kernel_amplitude": 0.2,
+            # "kernel_length_scale": 0.3,
+            "peak_value":3e-4,
+            "decay_rate":0.5,
+        }
+
+  
+
+
+  config.wandb_project_name = ''
+
+  config.sweep_configuration = ml_collections.ConfigDict()
+  config.sweep_configuration.method = "bayes"
+
+  config.sweep = True
+  config.sweep_id = ''
+  config.n_sweep_runs = 10
+  config.wandb_evaleta  = 1.0
+  config.sweep_configuration.metric = {"goal": "minimize", "name": "min_WD_joint_vs_MCMC"} # f"mean_dist_anchor_val_eta{config.wandb_evaleta}"}
+
+  config.sweep_configuration.parameters = {
+            # "kernel_amplitude": { "min": 0.03, "max": 1.0},
+            # "kernel_length_scale": { "min": 0.03, "max": 1.0},
+            "peak_value":{ "min": 1e-5, "max": 1e-2},
+            "decay_rate":{ "min": 0.1, "max": 1.0},
+        }
+  
+
+
+  return config
--- run_new.sh
+++ run_new.sh
@@ -14,7 +14,7 @@ WORK_DIR=$HOME/mount/vmp-output/lp-small
 mkdir -p $WORK_DIR
 
 
-########## 5 ITEMS
+########## 5 ITEMS ##########
 
 ## MCMC
 
@@ -28,9 +28,40 @@ mkdir -p $WORK_DIR
 
 MCMC_5_ITEMS_DIR=$HOME/mount/vmp-output/lp-small/5_items/mcmc/w500_s50_000_t50_sub100_eta_floating_1.0
 
+# Run the three scripts below if you want to do VI HPO
+## HPO AdditiveVMP eta only
 
+python3 $REPO_DIR/main.py --config $REPO_DIR/configs/5_items_hpo_flow_nsf_vmp_flow_etaAdditiveVMP.py \
+                          --config.path_mcmc_img $MCMC_5_ITEMS_DIR/lalme_floating_profiles_grid_eta_floating_1.000.png \
+                          --config.path_MCMC_samples $MCMC_5_ITEMS_DIR/lalme_az_w500_s50000_t50_sub100.nc \
+                          --config.wandb_project_name "LP-5items-HPO-AdditiveVMP-eta" \
+                          --config.n_sweep_runs 10 \
+                          --workdir $WORK_DIR/5_items/additive_vmp_eta/hpo \
+                          --log_dir $WORK_DIR/5_items/additive_vmp_eta/hpo/log_dir \
+                          --alsologtostderr
+# ## HPO VMP eta only
 
-# ## AdditiveVMP eta only
+# python3 $REPO_DIR/main.py --config $REPO_DIR/configs/5_items_hpo_flow_nsf_vmp_flow_etaVMP.py \
+#                           --config.path_mcmc_img $MCMC_5_ITEMS_DIR/lalme_floating_profiles_grid_eta_floating_1.000.png \
+#                           --config.path_MCMC_samples $MCMC_5_ITEMS_DIR/lalme_az_w500_s50000_t50_sub100.nc \
+#                           --config.wandb_project_name "LP-5items-HPO-VMP-eta" \
+#                           --config.n_sweep_runs 10 \
+#                           --workdir $WORK_DIR/5_items/vmp_eta/hpo \
+#                           --log_dir $WORK_DIR/5_items/vmp_eta/hpo/log_dir \
+#                           --alsologtostderr
+
+# ## HPO VP eta = 1.0
+
+# python3 $REPO_DIR/main.py --config $REPO_DIR/configs/5_items_hpo_flow_nsf_vmp_flow_VPeta1.py \
+#                           --config.path_mcmc_img $MCMC_5_ITEMS_DIR/lalme_floating_profiles_grid_eta_floating_1.000.png \
+#                           --config.path_MCMC_samples $MCMC_5_ITEMS_DIR/lalme_az_w500_s50000_t50_sub100.nc \
+#                           --config.wandb_project_name "LP-5items-HPO-VP-eta1" \
+#                           --config.n_sweep_runs 10 \
+#                           --workdir $WORK_DIR/5_items/vp_eta1/hpo \
+#                           --log_dir $WORK_DIR/5_items/vp_eta1/hpo/log_dir \
+#                           --alsologtostderr
+
+# ## RUN AdditiveVMP eta only
 
 # python3 $REPO_DIR/main.py --config $REPO_DIR/configs/5_items_flow_nsf_vmp_flow_etaAdditiveVMP.py \
 #                           --config.path_mcmc_img $MCMC_5_ITEMS_DIR/lalme_floating_profiles_grid_eta_floating_1.000.png \
@@ -40,7 +71,7 @@ MCMC_5_ITEMS_DIR=$HOME/mount/vmp-output/lp-small/5_items/mcmc/w500_s50_000_t50_s
 #                           --log_dir $WORK_DIR/5_items/additive_vmp_eta/log_dir \
 #                           --alsologtostderr
 
-# ## VMP eta only
+# ## RUN VMP eta only
 
 # python3 $REPO_DIR/main.py --config $REPO_DIR/configs/5_items_flow_nsf_vmp_flow_etaVMP.py \
 #                           --config.path_mcmc_img $MCMC_5_ITEMS_DIR/lalme_floating_profiles_grid_eta_floating_1.000.png \
@@ -50,7 +81,7 @@ MCMC_5_ITEMS_DIR=$HOME/mount/vmp-output/lp-small/5_items/mcmc/w500_s50_000_t50_s
 #                           --log_dir $WORK_DIR/5_items/vmp_eta/log_dir \
 #                           --alsologtostderr
                           
-# ## VP eta = 1.0
+# ## RUN VP eta = 1.0
 
 # python3 $REPO_DIR/main.py --config $REPO_DIR/configs/5_items_flow_nsf_vmp_flow_VPeta1.py \
 #                           --config.path_mcmc_img $MCMC_5_ITEMS_DIR/lalme_floating_profiles_grid_eta_floating_1.000.png \
@@ -60,13 +91,14 @@ MCMC_5_ITEMS_DIR=$HOME/mount/vmp-output/lp-small/5_items/mcmc/w500_s50_000_t50_s
 #                           --log_dir $WORK_DIR/5_items/vp_eta1/log_dir \
 #                           --alsologtostderr
 
+
 ## MCMC WD comparisons
 
-VI_path_dict="{'VMP':'$WORK_DIR/5_items/vmp_eta/lalme_az_eta_1.000.nc','ADDITIVE-VMP':'$WORK_DIR/5_items/additive_vmp_eta/lalme_az_eta_1.000.nc','VP':'$WORK_DIR/5_items/vp_eta1/lalme_az_eta_1.000.nc'}"
-eta_mcmc=1.0
-python3 $REPO_DIR/main.py --config $REPO_DIR/configs/5_items_mcmc.py \
-                        --workdir $WORK_DIR/5_items/mcmc/w500_s50_000_t50_sub100_eta_floating_$eta_mcmc \
-                        --config.path_variational_samples $VI_path_dict \
-                        --config.eta_profiles_floating "$eta_mcmc" \
-                        --log_dir $WORK_DIR/5_items/mcmc/w500_s50_000_t50_sub100_eta_floating_$eta_mcmc/log_dir \
-                        --alsologtostderr
+# VI_path_dict="{'VMP':'$WORK_DIR/5_items/vmp_eta/lalme_az_eta_1.000.nc','ADDITIVE-VMP':'$WORK_DIR/5_items/additive_vmp_eta/lalme_az_eta_1.000.nc','VP':'$WORK_DIR/5_items/vp_eta1/lalme_az_eta_1.000.nc'}"
+# eta_mcmc=1.0
+# python3 $REPO_DIR/main.py --config $REPO_DIR/configs/5_items_mcmc.py \
+#                         --workdir $WORK_DIR/5_items/mcmc/w500_s50_000_t50_sub100_eta_floating_$eta_mcmc \
+#                         --config.path_variational_samples $VI_path_dict \
+#                         --config.eta_profiles_floating "$eta_mcmc" \
+#                         --log_dir $WORK_DIR/5_items/mcmc/w500_s50_000_t50_sub100_eta_floating_$eta_mcmc/log_dir \
+#                         --alsologtostderr
