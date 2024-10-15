465b28fd7ee008d54455707f9f4b2e38cd6083d5
llaurabatt <laura.battaglia@keble.ox.ac.uk>
2024-10-15 16:58:54 +0000

    reorganize and import hpv and synth

--- data/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Datasets used to illustrate Bayesian Modular Inference."""
-
-from .load import load_lalme, process_lalme
-
-lalme_coarsen_8 = load_lalme(dataset_id='coarsen_8_items')
-lalme_coarsen_all = load_lalme(dataset_id='coarsen_all_items')
